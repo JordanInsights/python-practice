7910c6dc259443bc33f48e7a775548b5bf5cf6d1
Jordan Shaw <jordan.m.shaw@bjss.com>
2022-04-04 14:29:37 +0100

    Add the for loops notes

---
+++ for.py
@@ -0,0 +1,9 @@
+testList = ["Laurence", "Hello", "World", 55, 100, True]
+for item in testList:
+    print(item)
+
+myStr = 'Hello World'
+for letter in myStr:
+    if letter == ' ':
+        break
+    print(letter)

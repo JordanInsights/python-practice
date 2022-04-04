b5b9b4c199e2d553a0f71dd170acb328efb2fb93
Jordan Shaw <jordan.m.shaw@bjss.com>
2022-04-04 14:53:06 +0100

    Add the built-in functionality notes

---
+++ built-in-functionality.py
@@ -0,0 +1,8 @@
+# can use min and max to get values from collection
+testTuple = (1, 2, 500, 3476238, 100000000)
+print(min(testTuple))
+print(max(testTuple))
+
+# can use round to round numbers
+val = 4.5677
+print(round(val))

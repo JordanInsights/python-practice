028094c694f56196648da893d2d51128ff1575d8
Jordan Shaw <jordan.m.shaw@bjss.com>
2022-04-04 14:20:33 +0100

    Add the loops file

---
+++ loops.py
@@ -0,0 +1,13 @@
+val = 0
+while val < 10:
+    val += 1
+
+    if val == 2:
+        continue
+
+    # if val == 5:
+    #     break
+
+    print(val)
+else:
+    print('Val is no longer less than 10')
